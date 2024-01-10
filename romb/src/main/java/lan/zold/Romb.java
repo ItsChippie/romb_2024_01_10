cd455c34005b9c7ec42ac61c9efb37c44d9d247d
Miklós Rajmund <pacmagic75@gmail.com>
2024-01-10 11:30:44 +0100

    kezdes

---
+++ romb/src/main/java/lan/zold/Romb.java
@@ -0,0 +1,11 @@
+package lan.zold;
+
+public class Romb {
+    public double calcArea(double alpha) {
+        return 2704.3399073916876;
+    }
+
+    public double calcRange(double side) {
+        return 220.0;
+    }
+}
