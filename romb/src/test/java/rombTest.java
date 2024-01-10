653579b45d547d478b7834fa4165eea1d542e732
Miklós Rajmund <pacmagic75@gmail.com>
2024-01-10 11:37:16 +0100

    teszt3

--- romb/src/test/java/rombTest.java
+++ romb/src/test/java/rombTest.java
@@ -19,13 +19,13 @@ public class rombTest {
     }
     @Test
     public void testCalcRange() {
-        double actual2 = this.romb.calcRange(55.0);
-        double expected2 = 220.0;
+        double actual2 = this.romb.calcRange(59.0);
+        double expected2 = 236.0;
         Assert.assertEquals(actual2, expected2, 0.1, "Hiba! A megadott érték nem egyezik a várt értékkel!");
     }
     @Test
     public void testCalcAreaNotWorking() {
-        double actual3 = this.romb.calcArea(90.0);
+        double actual3 = this.romb.calcArea(27.0);
         double expected3 = 1.0;
         Assert.assertEquals(actual3, expected3, 0.1, "Hiba! A megadott érték nem egyezik a várt értékkel!");
     }
